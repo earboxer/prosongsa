498cd644fcc4201bfa08d5720e9a1292eac9a485
Zach DeCook <zachdecook@gmail.com>
2018-03-22 15:43:09 -0400

    * Simplify HTML

--- index.php
+++ index.php
@@ -5,22 +5,20 @@ index.php contains the main html used for creating the page.
 <html>
 	<head>
 		<!-- Latest compiled and minified CSS -->
-		<!--<link rel="stylesheet" href="../shared/bootstrap-3.3.6/css/bootstrap.min.css"
-			integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
-			crossorigin="anonymous"> -->
 		<link rel="stylesheet"
-			href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+			href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+			integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+			crossorigin="anonymous">
 
 		<link rel="stylesheet" href="index.css" >
-		<meta  http-equiv="Content-Type" content="text/html;  charset=iso-8859-1">
+		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=.65">
 		<title>Choruses and Hymns</title>
 	</head>
 
-  <p><body>
+<body class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
 
-<!-- Light yellow box around page contents -->
-<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-12 col-xs-12 bg-success">
+<header>
 
 	<div class = "col-xs-6 col-xs-offset-0 ">
 		<h3>Browse Songs</h3>
@@ -41,12 +39,9 @@ index.php contains the main html used for creating the page.
 		<div id="messages"></div>
 	</div>
 
-	<div class = "col-sm-0 col-md-2 col-lg-2"><p></p></div>
-	<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12"><p></p></div>
+</header>
 
-	<br></br>
-</div>
-<div class = "text-center col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-12 col-xs-12 bg-info ">
+<div class = "text-center">
 	<form>
 	<?php $transp = isset( $_GET['transp']) ? (int)$_GET['transp'] : 0 ?>
 	<select name="transp" id="transp"
@@ -64,20 +59,12 @@ index.php contains the main html used for creating the page.
 	</select>
 	<noscript>
 		<button>Transpose</button>
-	<?php
-		//$val1 = ($_GET['transp'] - 1 + 12)%12;
-		$val2 = ($transp + 2)%12;
-		//$val3 = ($_GET['transp'] + 3)%12;
-		//echo "<input type='submit' value='$val1' name='transp'>";
-		echo "<input type='submit' value='$val2' name='transp'>";
-		//echo "<input type='submit' value='$val3' name='transp'>";
-	?>
 	</noscript>
 	</form>
 </div>
 
 
-<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-12 col-xs-12 bg-info">
+<div>
 
 <?php
 include 'page.php';
@@ -95,16 +82,17 @@ include 'page.php';
 	}
 
 ?>
-<br/><br><br>
+</div>
+
+<footer>
 All songs are owned by their respective copyright holders. No infringement intended.
 <br>
 Powered by <a href='https://github.com/earboxer/prosongsa'>Prosongsa</a>.
 Suggest features <a href='https://github.com/earboxer/prosongsa/issues'>here</a>.
 <br>
 Licensed under the <a href='LICENSE'>GNU AGPLv3</a>. View source <a href='source.php'>here</a>.
-</div>
+</footer>
 
-<!-- <script type="text/javascript" src="../scripts/jqm.js"></script> -->
 <script   src="https://code.jquery.com/jquery-1.12.4.min.js"
 integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
 crossorigin="anonymous"></script>
